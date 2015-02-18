c33c65f59c6b537cc09a42806482e5d4a9dd1019
J. Stuart McMurray <kd5pbo@gmail.com>
2015-02-18 17:01:59 -0500

    Initial commit

---
+++ shift/tx.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * tx.go
+ * Goroutine to send data to insert
+ * by J. Stuart McMurray
+ * created 20150116
+ * last modified 20150116
+ *
+ * Copyright (c) 2014 J. Stuart McMurray <kd5pbo@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* Reads from the tun device and sends the data to insert. errors will be
+reported on echan.  The goroutine will terminate when dchan is closed. */
+func tx(tun Tunnel, insert *Insert, echan chan error) {
+	for {
+		/* Read a frame from the tun device */
+		fmt.Printf("Waiting on a frame.\n")
+		f, err := tun.Read()
+		if nil != err {
+			echan <- err
+			return
+		}
+		/* Drop frames that are too big */
+		/* Send frame to insert */
+		fmt.Printf("Frame (%v): %02X\n", len(f), f) /* DEBUG */
+	}
+}
