be372e7e1fde6a1a48a1e18030852e1e25c6f15e
J. Stuart McMurray <kd5pbo@gmail.com>
2015-02-25 20:37:04 -0500

    Removed commented bits

--- insert/sha2.h
+++ insert/sha2.h
@@ -57,9 +57,6 @@
 
 #ifndef SHA2_TYPES
 #define SHA2_TYPES
-//typedef unsigned char uint8;
-//typedef unsigned int  uint32;
-//typedef unsigned long long uint64;
 typedef uint8_t uint8;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
@@ -75,14 +72,6 @@ typedef struct {
     uint32 h[8];
 } sha256_ctx;
 
-//typedef struct {
-//    unsigned int tot_len;
-//    unsigned int len;
-//    unsigned char block[2 * SHA512_BLOCK_SIZE];
-//    uint64 h[8];
-//} sha512_ctx;
-//
-//typedef sha512_ctx sha384_ctx;
 typedef sha256_ctx sha224_ctx;
 
 void sha224_init(sha224_ctx *ctx);
@@ -90,27 +79,6 @@ void sha224_update(sha224_ctx *ctx, const uint8_t *message,
                    unsigned int len);
 void sha224_final(sha224_ctx *ctx, uint8_t *digest);
 void sha224(const uint8_t *message, unsigned int len, uint8_t *digest);
-
-//void sha256_init(sha256_ctx * ctx);
-//void sha256_update(sha256_ctx *ctx, const unsigned char *message,
-//                   unsigned int len);
-//void sha256_final(sha256_ctx *ctx, unsigned char *digest);
-//void sha256(const unsigned char *message, unsigned int len,
-//            unsigned char *digest);
-
-//void sha384_init(sha384_ctx *ctx);
-//void sha384_update(sha384_ctx *ctx, const unsigned char *message,
-//                   unsigned int len);
-//void sha384_final(sha384_ctx *ctx, unsigned char *digest);
-//void sha384(const unsigned char *message, unsigned int len,
-//            unsigned char *digest);
-//
-//void sha512_init(sha512_ctx *ctx);
-//void sha512_update(sha512_ctx *ctx, const unsigned char *message,
-//                   unsigned int len);
-//void sha512_final(sha512_ctx *ctx, unsigned char *digest);
-//void sha512(const unsigned char *message, unsigned int len,
-//            unsigned char *digest);
 
 #ifdef __cplusplus
 }
